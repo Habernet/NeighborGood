71cfb1eb9cc90730ba85b3126a49164dcd8f0399
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-08-03 13:01:19 -0400

    controllers and routes in place

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +13,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here instead of in a separate file, we will keep things simple for this project.
+// Add routes, both API and view
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.Mongodb_URI || "mongodb://localhost/neighborgood");
