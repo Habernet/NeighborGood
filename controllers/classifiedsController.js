968648b07c92fa6419027c71df5f3de0a5f6f755
habernet <phaberern@gmail.com>
2019-08-06 18:01:43 -0400

    Controllers fixed, routes tested. Scripts written and added to package

--- controllers/classifiedsController.js
+++ controllers/classifiedsController.js
@@ -2,33 +2,28 @@ const db = require("../models/");
 
 module.exports = {
   findAll: function(req, res) {
-    db.classified
-      .find(req.query)
+    db.Classified.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.classified
-      .findById(req.params.id)
+    db.Classified.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.classified
-      .create(req.body)
+    db.Classified.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.classified
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.Classified.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.classified
-      .findById({ _id: req.params.id })
+    db.Classified.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -2,33 +2,28 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    db.event
-      .find(req.query)
+    db.Event.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.event
-      .findById(req.params.id)
+    db.Event.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.event
-      .create(req.body)
+    db.Event.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.event
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.Event.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.event
-      .findById({ _id: req.params.id })
+    db.Event.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
