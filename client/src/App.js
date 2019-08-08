b2625b84b96b566fdba9f11ff2d038fcfccda685
tim-garner <timothy.garner602@gmail.com>
2019-08-08 19:26:42 -0400

    updated front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./component/Navbar/navbar";
-import Jumbotron from "./component/Jumbotron/jumbotron";
-import About from "./component/About/about";
-import Footer from "./component/Footer/footer";
+import Navbar from "./components/Navbar/navbar";
+import Jumbotron from "./components/Jumbotron/jumbotron";
+import About from "./components/About/about";
+import Footer from "./components/Footer/footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-
+import Home from "./components/Home/home";
 function App() {
   return (
   
         <Router>
           <div>
+ 
       <Navbar />
+
+      <Home backgroundImage="./images/nbrhood.jpg">
+        <h1>NeighborGood</h1>
+        <h2>Come check out our events!</h2>
+      </Home>
 
       </div>
       <Jumbotron />
