eea9a20cbd3dedacbd1866ee0750ff354782826c
pcharpia6 <46582563+pcharpia6@users.noreply.github.com>
2019-08-13 18:35:21 -0400

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -23,7 +23,7 @@ module.exports = {
     console.log(req.body);
     db.User.create(req.body)
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err + "Ya frikkin ijit"));
+      .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     // db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
