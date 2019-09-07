9439b4f3610a5c3fa6e8722e4894ad02afef0357
habernet <phaberern@gmail.com>
2019-09-07 11:21:40 -0400

    Contact page works now, hidden character issue resolved

--- client/src/pages/contacts.js
+++ client/src/pages/contacts.js
@@ -1,31 +1,33 @@
-// import React,{Component} from 'react';
-// import ContactForm from '../components/ContactPage/contactform';
-// import { Row, Col,Container } from "../components/Grid";
-// import Button from '../components/Button';
-// class  ContactUser extends Component {
-//   handleClick = event => {
-//   event.preventDefault();
-//     window.location.href = "mailto:xyz@example.com?subject=NEIGHBORGOOD Email!!!  Contact us ";
-//   }
-// ​
-//  render(){
-//   return (
-// ​
-//     <Container>
-//     <Row>
-//       <div style={{"margin-right": "auto", "margin-left": "auto","padding":"200px"}}>
-//       <Col size="md-12">
-//       <ContactForm
-//                 formSubmit={this.handleClick}
-//               />
+import React, { Component } from "react";
+import ContactForm from "../components/ContactForm/contactform";
+import { Row, Col, Container } from "../components/Grid";
+class ContactUser extends Component {
+  handleClick = event => {
+    event.preventDefault();
+    console.log("YOU CLICKED THE CONTACT US BUTTON!");
+    window.location.href =
+      "mailto:xyz@example.com?subject=NEIGHBORGOOD Email!!!  Contact us ";
+  };
 
-//       </Col>
-//       </div>
-//       </Row>
-//       </Container>
-//   );
-// }
-// }
-// ​
+  render() {
+    return (
+      <Container>
+        <Row>
+          <div
+            style={{
+              "margin-right": "auto",
+              "margin-left": "auto",
+              padding: "200px"
+            }}
+          >
+            <Col size="md-12">
+              <ContactForm formSubmit={this.handleClick} />
+            </Col>
+          </div>
+        </Row>
+      </Container>
+    );
+  }
+}
 
-// export default ContactUser;
+export default ContactUser;
